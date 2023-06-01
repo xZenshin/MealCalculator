c2110cafef87e40de4c88ce1387aea4976dcaaf9
Alexander Skou-Larsen <askoularsen@gmail.com>
2023-06-01 23:59:13 +0200

    Idk intial classes i guess

---
+++ MealCalculator/Ingredient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MealCalculator
+{
+    public class Ingredient
+    {
+        private string Name;
+        private double Calories;
+        private double Protein;
+        private double Fat;
+        private double Carbs;
+
+        public Ingredient(string name, double calories, double protein, double fat, double carbs)
+        {
+            Name = name;
+            Calories = calories;
+            Protein = protein;
+            Fat = fat;
+            Carbs = carbs;
+        }
+
+        public string getName() { return Name; }
+        public double getCalories() { return Calories; }
+        public double getProtein() {  return Protein; }
+        public double getFat() { return Fat; } 
+        public double getCarbs() {  return Carbs; }
+    }
+}
---
+++ MealCalculator/Meal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MealCalculator
+{
+    public enum MealType
+    {
+        Breakfast,
+        Lunch,
+        Dinner,
+    }
+
+    public class Meal
+    {
+        private MealType Type;
+        private string Name;
+        private Array Ingredients;
+
+        public Meal(MealType type, string name, Array ingredients)
+        {
+            this.Type = type;
+            this.Name = name;
+            this.Ingredients = ingredients;
+        }
+
+        public MealType getMealType() { return Type; }
+        public string getName() { return Name; }
+        public Array getIngredients() {  return Ingredients; }
+    }
+}
