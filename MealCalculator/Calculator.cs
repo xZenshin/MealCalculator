da1e2500465f67cbcb207e419bdec5e62bd7827a
Alexander Skou-Larsen <askoularsen@gmail.com>
2023-06-05 19:29:15 +0200

    add calculator save and load to binary doesnt work tho

--- MealCalculator/Calculator.cs
+++ MealCalculator/Calculator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +9,50 @@ namespace MealCalculator
 {
     public class Calculator
     {
+        private Dictionary<string, NutritionInfo> nutritionDict;
 
         public Calculator() { }
 
+        public void Save()
+        {
+            try
+            {
+                WriteToBinaryFile("NutritionInfo", nutritionDict);
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
 
+        public void Load()
+        {
+            try
+            {
+                Dictionary<string, NutritionInfo> temp = ReadFromBinaryFile<Dictionary<string, NutritionInfo>>("NutritionInfo");
+                this.nutritionDict = temp;
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+  
+        }
+
+        private static void WriteToBinaryFile<T>(string filePath, T objectToWrite, bool append = false)
+        {
+            using (Stream stream = File.Open(filePath, append ? FileMode.Append : FileMode.Create))
+            {
+                var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                binaryFormatter.Serialize(stream, objectToWrite);
+            }
+        }
+
+        private static T ReadFromBinaryFile<T>(string filePath)
+        {
+            using (Stream stream = File.Open(filePath, FileMode.Open))
+            {
+                var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                return (T)binaryFormatter.Deserialize(stream);
+            }
+        }
     }
 }
